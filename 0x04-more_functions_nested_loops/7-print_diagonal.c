aad91f5b0202c52426a14c97315b199e3383e002
Okolie Samuel <uche7877@gmail.com>
2022-09-15 11:59:11 -0700

    fixed bugs

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,7 @@ void print_diagonal(int n)
 
 {
 	int co, sp;
-	
+
 	if (n <= 0)
 	{
 		_putchar('\n');
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@ void print_square(int size)
 
 {
 	int co, ro;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
