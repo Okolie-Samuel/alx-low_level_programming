fdb39c045684a29e8e3e7921a51cda8da2ed45d2
Okolie Samuel <uche7877@gmail.com>
2022-10-06 12:45:49 -0700

    dynamic allocation using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - concatenates two strings.
+ * @s1: pointer to first string.
+ * @s2: pointer to second string from which we'll take first n bytes.
+ * @n: n bytes of s2.
+ * Return: pointer to newly allocated space in memory.
+ * Or NULL if the function falis
  */
+
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *concat;
+	unsigned int l = n, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+	for (i = 0; s1[i]; i++)
+		l++;
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	concat = malloc(sizeof(char) * (l + 1));
 
-	if (n < j)
-		j = n;
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	if (concat == NULL)
 		return (NULL);
+	l = 0;
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	for (i = 0; s1[i]; i++)
+		concat[l++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		concat[l++] = s2[i];
+
+	concat[l] = '\0';
+
+	return (concat);
 }
