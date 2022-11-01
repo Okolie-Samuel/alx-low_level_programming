beec3c2694b93d2ca61fd379491fdd7f50226219
Okolie Samuel <uche7877@gmail.com>
2022-10-31 19:53:56 -0700

    input and output file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,54 @@
-#include "main.h"
+#include <sys/types.h>
 
+#include <sys/stat.h>
 
+#include <fcntl.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints it to stdout
- * @filename: the file name to read from
- * @letters: the number of characters it should read and print
- * Return: the actual number of characters it could read and print
- * or 0 if error
+ * read_textfile - reads a text file and prints it to the Stdout
+ * @filename: path od a filename
+ * @letters: number to letters to read and write
+ * Return: 0 if 'filename is NULL or cannot be open, read or write
+ * or number of bytes used to call write
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-	int f, l, i;
-	int r;
 	char *buf;
+	int fd;
+	ssize_t r, w;
 
 	if (filename == NULL)
 		return (0);
-	/* open file */
-
-	f = open(filename, O_RDONLY);
-
-	if (f == -1)
-		return (0);
-
 	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	if (buf == NULL)
 		return (0);
-
-	/* read in buf */
-	read(f, buf, letters);
-	buf[letters] = '\0';
-
-	for (i = 0; buf[i] != '\0'; i += 1)
-		l += 1;
-	/* close file*/
-	re = close(f);
-	if (r != 0)
-		exit(-1);
-	r = write(STDOUT_FILENO, buf, l);
-	if (r != l)
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		free(buf);
 		return (0);
-	free(buf);
-
-	return (l);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (w != r)
+		return (0);
+	return (r);
 }
